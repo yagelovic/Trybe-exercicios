da65f0486fae6ac660ccc9544dacad41f30590cd
Paulo <paulojose.yagelovic@gmail.com>
2022-07-21 19:07:26 -0300

    atualiza js solucionando exercicio 3

--- fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/4.2.js
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/4.2.js
@@ -10,10 +10,23 @@
 
 // Questão 2:
 
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+// resultado += numbers[index];
+// }
+// console.log(resultado);
+
+// Questão 3:
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let resultado = 0;
+let divisor = numbers.length;
+
 
 for (let index = 0; index < numbers.length; index += 1) {
-resultado += numbers[index];
+    resultado += numbers[index];
 }
-console.log(resultado);
+let media = resultado / divisor;
+console.log(media);
