17ef454aa1727a2f3638dea8ee8550db72b835c9
Paulo <paulojose.yagelovic@gmail.com>
2022-07-21 18:50:58 -0300

    atualiza js solucionando exercicio 2

--- fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/4.2.js
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/4.2.js
@@ -1,4 +1,19 @@
+// Questão 1:
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let index = 0; index < numbers.length; index += 1) {
+
+//     console.log(numbers[index]);
+// }
+
+//----------------------------------------------------------
+
+// Questão 2:
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
+
 for (let index = 0; index < numbers.length; index += 1) {
-    console.log(numbers[index]);
+resultado += numbers[index];
 }
+console.log(resultado);
