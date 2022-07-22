5e07b8e65fe39b70dd5e42c3f294cb8772943e0b
Paulo <paulojose.yagelovic@gmail.com>
2022-07-22 14:31:28 -0300

    atualiza js solucionando exercicio 7

--- fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/4.2.js
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/4.2.js
@@ -49,15 +49,26 @@
 
 // Questão 6:
 
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let contador = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] % 2 != 0) {
+//         contador += 1;
+//     }
+// }
+// if (contador === 0) {
+//     console.log("Nenhum valor ímpar encontrado!");
+// }   else {
+//     console.log(contador, "números ímpar");
+// }
+
+// Questão 7:
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let contador = 0;
-for (let index = 0; index < numbers.length; index += 1) {
-    if (numbers[index] % 2 != 0) {
-        contador += 1;
+let menor = 100000;
+for (let index = 0; index < numbers.length - 1; index += 1) {
+    if (numbers[index] < menor) {
+        menor = numbers[index];
     }
 }
-if (contador === 0) {
-    console.log("Nenhum valor ímpar encontrado!");
-}   else {
-    console.log(contador, "números ímpar");
-}
+console.log(menor);
