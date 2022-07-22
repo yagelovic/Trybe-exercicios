ae3d07ba9c7213f044fd743cbf0bed4f62cde976
Paulo <paulojose.yagelovic@gmail.com>
2022-07-22 14:12:26 -0300

    atualiza js solucionando exercicio 5

--- fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/4.2.js
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/4.2.js
@@ -18,20 +18,31 @@
 // }
 // console.log(resultado);
 
-// Questão 3:
+// Questão 3 e 4:
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0;
+// let divisor = numbers.length;
+
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     resultado += numbers[index];
+// }
+// let media = resultado / divisor;
+// console.log(media);
+// if (media > 20) {
+// console.log("Valor maior que 20");
+// } else {
+//     console.log("Valor menor ou igual a 20");
+// }
+
+// Questão 5:
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
-let divisor = numbers.length;
-
-
-for (let index = 0; index < numbers.length; index += 1) {
-    resultado += numbers[index];
+let maior = 0;
+for (let index = 0; index < numbers.length-1; index += 1) {
+if (numbers[index] > maior) {
+    maior = numbers[index];
 }
-let media = resultado / divisor;
-console.log(media);
-if (media > 20) {
-console.log("Valor maior que 20");
-} else {
-    console.log("Valor menor ou igual a 20");
 }
+console.log(maior);
