5371629d94115bfecf5a78f80b3ec43eb782a205
Paulo <paulojose.yagelovic@gmail.com>
2022-07-25 15:57:31 -0300

    atualiza .js com a resolução dos exercicios 3 e 4 da parte de objetos

--- fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/dia-4-javaScript-objetos-e-funcoes/exercicios4-4.js
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/dia-4-javaScript-objetos-e-funcoes/exercicios4-4.js
@@ -4,13 +4,15 @@
 // let medals = { golden: 2, silver: 3 };
 
 let player = {
-    nome : 'Marta',
-    lastName : 'Silva',
-    age : 34,
-    medals : {
-        golden : 2,
-        silver : 3,
+    nome: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    bestInTheWorld: ['2006', '2007', '2008', '2009', '2010', '2018'],
+    medals: {
+        golden: 2,
+        silver: 3,
     }
 }
 
 console.log("A jogadora " + player['nome'] + ' ' + player['lastName'] + ' tem ' + player['age'] + ' anos de idade.' )
+console.log("A jogadora " + player['nome'] + ' ' + player['lastName'] + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.')
